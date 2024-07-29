86a91fa2ff376005092eec4dd6d99d9f47aad75d
Guru KN <guruprasadkn1991@gmail.com>
2024-07-29 09:51:54 +0530

    added condition in login page

--- src/app/login-button/login-button.component.ts
+++ src/app/login-button/login-button.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-button',
@@ -7,9 +8,16 @@ import { AuthService } from '@auth0/auth0-angular';
   styles: [],
 })
 export class LoginButtonComponent implements OnInit {
-  constructor(public auth: AuthService) {}
+  constructor(public auth: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.auth.user$.subscribe((user: any) => {
+      if(user){
+        console.log('User Profile:', user);
+        this.router.navigate(['/home']);
+      }
+    });
+  }
 
   loginWithRedirect(): void {
     this.auth.loginWithRedirect();
